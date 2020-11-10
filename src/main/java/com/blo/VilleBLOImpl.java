4c6d3670e80772dfcd09e2cd6f29bc30703ace51
clogi <clogi@LAPTOP-UV8IFMGL>
2020-11-10 15:37:54 +0100

    [Commit 1]

---
+++ src/main/java/com/blo/VilleBLOImpl.java
@@ -0,0 +1,20 @@
+package com.blo;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import com.dto.Ville;
+
+@Service
+public class VilleBLOImpl implements VilleBLO {
+
+	@Override
+	public ArrayList<Ville> getInfoVille() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
