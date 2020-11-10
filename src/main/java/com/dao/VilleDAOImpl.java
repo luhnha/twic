c6953df55eff97e2c4c0bec40ad55c30c11efdf2
clogi <clogi@LAPTOP-UV8IFMGL>
2020-11-10 16:28:50 +0100

    [Correction] nom classes

--- src/main/java/com/dao/VilleDAOImpl.java
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -1,9 +1,20 @@
 package com.dao;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Repository;
+
+import com.dto.Ville;
 
 @Repository
 public class VilleDAOImpl implements VilleDAO{
+
+	@Override
+	public ArrayList<Ville> getInfoVille() {
+		ArrayList<Ville> listVille = new ArrayList<Ville>();
+		// TODO Auto-generated method stub
+		return listVille;
+	}
 
 	
 }
